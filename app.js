05bc50f98d6bf93396d7eafd786afd2f4bd7e7e4
Oksana Donchuk <80112131+OksanaDonchuk@users.noreply.github.com>
2024-06-04 00:35:33 +0300

    Add beginning file

---
+++ app.js
@@ -0,0 +1,45 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+import e from "cors";
+
+const { DB_HOST, PORT } = process.env;
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+app.use("/api/users");
+app.use("/api/categories");
+app.use("/api/areas");
+app.use("/api/ingredients");
+app.use("/api/testimonials");
+app.use("/api/recipes");
+
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = "Server error" } = err;
+  res.status(status).json({ message });
+});
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running. Use our API on port: ${PORT}`);
+    });
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
